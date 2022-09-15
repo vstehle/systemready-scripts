cb9f53f4a37a56a7db0a53381a4969fb5f4262c5
Vincent Stehlé <vincent.stehle@arm.com>
2022-09-15 16:40:58 +0200

    validate.py: add support for referencing local files
    Enhance the validate.py script to allow referencing other schemas from a
    schema, and load the local file with the script.
    
    This will allow us to factorize our schemas.
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>

--- validate.py
+++ validate.py
@@ -4,6 +4,8 @@ import argparse
 import sys
 import yaml
 import jsonschema
+import os
+import logging
 
 try:
     from packaging import version
@@ -17,22 +19,49 @@ if 'packaging.version' in sys.modules and \
 else:
     yaml_load_args = {}
 
+# The folder of the schema.
+schema_folder = None
+
+
+def load_yaml(filename):
+    logging.debug(f"Loading `{filename}'")
+    with open(filename, 'r') as f:
+        return yaml.load(f, **yaml_load_args)
+
+
+def handler(uri):
+    f = f"{schema_folder}/{os.path.basename(uri)}"
+    logging.debug(f"Resolving URI `{uri}' as `{f}'")
+    return load_yaml(f)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Validate a YAML file with a schema.')
+    parser.add_argument(
+        '--debug', action='store_true', help='Turn on debug messages')
     parser.add_argument(
         '--schema', required=True, help='Schema file to use for validation')
     parser.add_argument('yamlfile', help='YAML file to validate')
     args = parser.parse_args()
 
+    logging.basicConfig(
+        format='%(levelname)s %(funcName)s: %(message)s',
+        level=logging.DEBUG if args.debug else logging.INFO)
+
+    schema_folder = os.path.dirname(args.schema)
+
     # Load YAML file.
-    with open(args.yamlfile, 'r') as f:
-        data = yaml.load(f, **yaml_load_args)
+    data = load_yaml(args.yamlfile)
 
     # Load YAML jsonschema.
-    with open(args.schema, 'r') as f:
-        schema = yaml.load(f, **yaml_load_args)
+    schema = load_yaml(args.schema)
 
     fc = jsonschema.FormatChecker()
-    jsonschema.validate(instance=data, schema=schema, format_checker=fc)
+
+    resolver = jsonschema.validators.RefResolver.from_schema(
+        schema, handlers={'http': handler, 'https': handler})
+
+    jsonschema.validate(
+        instance=data, schema=schema, format_checker=fc, resolver=resolver)
     # If we arrive here, data is valid.
