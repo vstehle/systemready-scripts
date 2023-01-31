723c9e36bbdaea50889ee4d1387331b71a29f2d0
Vincent Stehlé <vincent.stehle@arm.com>
2023-01-31 17:02:53 +0100

    check-sr-results: improve ^h handling
    Our handling of control character is not perfect, but it is better to
    replay ^H characters one by one to better match what happens with a
    terminal. This improves a bit parsing of U-Boot logs.
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>

--- check-sr-results.py
+++ check-sr-results.py
@@ -290,7 +290,7 @@ def cleanup_line(line):
     line = re.sub(r'\x1B\[[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E]', '', line)
 
     while re.search(r'\x08', line):
-        line = re.sub(r'.?\x08', '', line)
+        line = re.sub(r'.?\x08', '', line, count=1)
 
     return line
 
