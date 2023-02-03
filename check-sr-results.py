90983d7cbfebbe9bae14925404b4bb08e89469f2
Vincent Stehlé <vincent.stehle@arm.com>
2023-02-03 18:55:14 +0100

    check-sr-results: ignore bell control char
    This slightly improve cleanup of the logs, which avoids emitting false
    positive errors.
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>

--- check-sr-results.py
+++ check-sr-results.py
@@ -314,6 +314,7 @@ def cleanup_line(line):
     line = line.rstrip()
     line = re.sub(r'\x1B\[K', '', line)
     line = re.sub(r'\x1B\(B', '', line)
+    line = re.sub(r'\x07', '', line)
     line = re.sub(r'\x1B\[[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E]', '', line)
 
     while re.search(r'\x08', line):
