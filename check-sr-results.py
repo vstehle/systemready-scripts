c5245c59e83ec14e158730c232930c4fd6aaf71e
Vincent Stehlé <vincent.stehle@arm.com>
2023-03-31 18:05:23 +0200

    check-sr-results: bump linux version to 6.2.9
    Signed-off-by: Vincent Stehlé <vincent.stehle@arm.com>

--- check-sr-results.py
+++ check-sr-results.py
@@ -1460,7 +1460,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--linux-url', help='Specify Linux tarball URL',
         default='https://cdn.kernel.org/pub/linux/kernel/v6.x/'
-                'linux-6.1.7.tar.xz')
+                'linux-6.2.9.tar.xz')
     parser.add_argument(
         '--parser', help='Specify (SCT) parser.py path',
         default='parser.py')
